ea2217605c3e18a6a86139441ffbd319b587e78d
Cláudio Roberto <claudioroberto0707@outlook.com>
2024-10-09 23:38:13 -0300

    conclusão do component Programacao e Design

--- src/components/Frontend.js
+++ src/components/Frontend.js
@@ -5,29 +5,33 @@ const Frontend = ({ livros }) => (
   <main className="principal">
     <h2>Categoria Frontend</h2>
     {livros
-      .filter(cat => cat.categoria === "frontend")
-      .map(livro => (
+      .filter((cat) => cat.categoria === "frontend")
+      .map((livro) => (
         <div className="card" key={livro.id}>
           <div className="thumb">
-            <img src={"/imagens/capas/" + livro.id + ".jpg"}
-              alt="Thumbnail da capa do livro..."
+            <img
+              src={"/imagens/capas/" + livro.id + ".jpg"}
+              alt="Thumbnail da capa do livro ..."
             />
           </div>
           {livros
-            .filter(c => c.slug === livro.slug)
-            .map(livro => (
+            .filter((c) => c.slug === livro.slug)
+            .map((livro) => (
               <span key={livro.slug}>
                 <Link to={`/livro/${livro.slug}`}>
-                  {< div className="detalhes">
-                    <h3>{livro.titulo}</h3>
-                    <p>{livro.descricao.slice(0, 130) + "..."}</p>
-                    <p>Leia Mais &gt;</p>
-                  </div>}
+                  {
+                    <div className="detalhes">
+                      <h3>{livro.titulo}</h3>
+                      <p>{livro.descricao.slice(0, 130) + "..."}</p>
+                      <p>Leia mais &gt;</p>
+                    </div>
+                  }
                 </Link>
               </span>
             ))}
         </div>
       ))}
   </main>
-)
+);
+
 export default Frontend;
--- src/components/Programacao.js
+++ src/components/Programacao.js
@@ -1,12 +1,33 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-const Programacao = () => (
+const Programacao = ({ livros }) => (
   <main className="principal">
-    <h2>Categoria programacao</h2>
-    <div className="card">
-      <p>Conteúdos da página programacao</p>
-    </div>
+    <h2>Categoria Programação</h2>
+    {livros
+      .filter(cat => cat.categoria === "programacao")
+      .map(livro => (
+        <div className="card" key={livro.id}>
+          <div className="thumb">
+            <img src={"/imagens/capas/" + livro.id + ".jpg"}
+              alt="Thumbnail da capa do livro..."
+            />
+          </div>
+          {livros
+            .filter(c => c.slug === livro.slug)
+            .map(livro => (
+              <span key={livro.slug}>
+                <Link to={`/livro/${livro.slug}`}>
+                  {< div className="detalhes">
+                    <h3>{livro.titulo}</h3>
+                    <p>{livro.descricao.slice(0, 130) + "..."}</p>
+                    <p>Leia Mais &gt;</p>
+                  </div>}
+                </Link>
+              </span>
+            ))}
+        </div>
+      ))}
   </main>
-);
-
+)
 export default Programacao;
