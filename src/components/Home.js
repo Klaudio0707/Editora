877e75be85d02102ba060f83e6d5acff36360f7e
Cláudio Roberto <claudioroberto0707@outlook.com>
2024-10-09 22:06:17 -0300

    component home feito

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,40 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-const Home = () => (
+  {/* // objeto livros é passado como um props dentro do componente home */}
+const Home = ({ livros }) => ( 
+  
+
   <main className="principal">
-    <h2>Últimos lançamentos</h2>
-    <div className="card">
-      <p>Conteúdos da Home Page</p>
-    </div>
+    <h2>Último lançamentos</h2>
+    {livros
+      .filter((n, index) => index < 8)
+      .map(livro => (
+        <div className="card" key={livro.id}>  {/* //a div card pega o id do livro */}
+          <div className="thumb">
+
+            <img src={"/imagens/capas/" + livro.isbn.replace(/-/g, "") + ".jpg"} alt="Thumbnail da capa do livro..." />
+
+          </div>
+          {livros
+            .filter(c => c.slug === livro.slug)
+            .map(livro => (
+              <Link to={`/livro/${livro.slug}`} key={livro.id}>
+                {
+                  <div className="detalhes">
+                    <h3>{livro.titulo}</h3>
+                    <p>{livro.descricao.slice(0, 130) + "..."}</p>
+                    <p>Leoa masi &gt;</p>
+                  </div>
+                }
+              </Link>
+            ))}
+        </div>
+      ))
+    }
+
   </main>
+
 );
 
 export default Home;
